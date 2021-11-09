942487576a28bd400cf5ccefc2af8597efbc0d42
yamato090 <tomoki.y.0924@icloud.com>
2021-11-09 12:10:48 +0000

    add 検索機能

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -48,6 +48,7 @@ class BooksController < ApplicationController
         redirect_to books_path
     end
 
+
     private
     def book_params
         params.require(:book).permit(:title, :body)
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
       resources :book_comments, only: [:create, :destroy]
   end
+  get '/search', to: 'searchs#search'
 end
 
 
